4a85b11d2fca4f6255e6bb7d1777a3d0dca460ae
zorozuro <yasinnew72@gmail.com>
2025-09-21 18:08:03 +0530

    Add CI/CD pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, World!");
+console.log("This is a sample Node.js application.");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Testing Pipeline");
+setTimeout(()=>{console.log("Waiting for 3 seconds...")},3000);
+console.log("Test Completed");
